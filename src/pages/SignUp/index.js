8ee054b29970dc0a74d5a06586263c6620caea61
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-11-23 15:03:09 -0300

    feat: add unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/logo.svg';
 
-export default function SignUp() {
-  return <h1>SignUp</h1>;
+export default function SignUn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Senha" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho conta</Link>
+      </Form>
+    </>
+  );
 }

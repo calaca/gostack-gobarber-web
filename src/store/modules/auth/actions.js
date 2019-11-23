49f14d4efca31e52a2c4f44ee6e1f9e403ccde1d
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-11-23 16:27:26 -0300

    feat: add auth

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,27 @@
+import constants from './constants';
+
+export function signInRequest(email, password) {
+  return {
+    type: constants.authSignInRequest,
+    payload: {
+      email,
+      password,
+    },
+  };
+}
+
+export function signInSuccess(token, user) {
+  return {
+    type: constants.authSignInSuccess,
+    payload: {
+      token,
+      user,
+    },
+  };
+}
+
+export function signFailure() {
+  return {
+    type: constants.authSignFailure,
+  };
+}
--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,7 +1,19 @@
-const INITIAL_STATE = {};
+import produce from 'immer';
+import constants from './constants';
+
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
 
 export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case constants.authSignInSuccess:
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }

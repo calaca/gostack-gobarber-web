fbca5347c30815059945bfef6d7dacd4b8d48cb4
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-11-23 21:08:00 -0300

    feat: add loading auth

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -9,10 +9,19 @@ const INITIAL_STATE = {
 
 export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case constants.authSignInRequest:
+      return produce(state, draft => {
+        draft.loading = true;
+      });
     case constants.authSignInSuccess:
       return produce(state, draft => {
         draft.token = action.payload.token;
         draft.signed = true;
+        draft.loading = false;
+      });
+    case constants.authSignFailure:
+      return produce(state, draft => {
+        draft.loading = false;
       });
     default:
       return state;

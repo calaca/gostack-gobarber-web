65a0055c25dd05414244f7c22c20424b1b9656ed
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-11-24 22:58:32 -0300

    feat: style profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome" />
+        <Input name="email" type="email" placeholder="Email" />
+        <hr />
+        <Input name="oldPassword" type="password" placeholder="Senha antiga" />
+        <Input name="password" type="password" placeholder="Senha nova" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Repita a senha nova"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }

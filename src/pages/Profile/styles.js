44f77782fda14893ce8d80be2bb57821451a17ed
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-12-02 22:44:57 -0300

    feat: list appointments

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -3,7 +3,8 @@ import { darken } from 'polished';
 
 export const Container = styled.div`
   max-width: 600px;
-  margin: 50px auto;
+  margin: 0 auto;
+  padding: 50px 0;
 
   form {
     display: flex;
